e15ad0d1b671b7bccedbfd1fc5814faf0007d96a
Iván Guerrero Alvarez <ivanguerreroa@gmail.com>
2024-03-08 19:47:41 -0300

    ultima acutlizacion

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
         alert("El correo fue enviado correctamente");
     })
 });
+
+/* Smootp Scrol */
+$('.carousel').on('slide.bs.carousel', function (e) {
+    var $target = $(e.relatedTarget);
+    var index = $target.index();
+    $('html, body').animate({
+      scrollTop: $target.offset().top
+    }, 800); // Duración del desplazamiento en milisegundos
+  });
